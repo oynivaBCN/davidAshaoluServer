5af65eb6a87a8fa6d6d32445985068d0c8f66c7c
pandorajk <pandora.knocker@psious.com>
2023-03-09 22:02:21 +0100

    feat: add to error-helper

--- services/error-helper/error-helper.js
+++ services/error-helper/error-helper.js
@@ -11,14 +11,19 @@ const ErrorHelper = {
 		NOT_AUTHORIZED: 'NotAuthorizedException',
 		INVALID_TOKEN: 'Invalid token specified',
 		EXPIRED_CODE: 'ExpiredCodeException',
+		INVALID_PASSWORD: 'InvalidPasswordException',
 	},
 
 	getErrorStatus(error) {
+		// console.log('error-helper:', error);
 		if (error.name === this.COGNITO_ERRORS.NOT_AUTHORIZED) {
 			return this.ERROR_STATUS_CODES.UNAUTHORIZED;
 		}
 		if (error.name === this.COGNITO_ERRORS.EXPIRED_CODE) {
 			return this.ERROR_STATUS_CODES.UNAUTHORIZED;
+		}
+		if (error.name === this.COGNITO_ERRORS.INVALID_PASSWORD) {
+			return this.ERROR_STATUS_CODES.BAD_REQUEST;
 		}
 
 		if (error.error?.message === this.COGNITO_ERRORS.INVALID_TOKEN) {
