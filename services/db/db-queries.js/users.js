fa4d276cd1aedbc3b4057e27e643b97adc8ceb5a
pandorajk <pandora.knocker@psious.com>
2023-05-10 12:31:24 +0200

    feat: remove sql injection vulnerability

--- services/db/db-queries.js/users.js
+++ services/db/db-queries.js/users.js
@@ -1,19 +1,21 @@
 const database = require('../db-connection');
 
 const getUsers = async () => {
-	const query = `SELECT * from davidashaolu.users`;
-	return await database.dbConn(query);
+	const query = `SELECT * from da.users;`;
+	return await database.connectToRds(query);
 };
 
 const getUserBySub = async (sub) => {
-	const query = `SELECT * from davidashaolu.users WHERE users.sub = "${sub}"`;
-	const result = await database.dbConn(query);
-	return result.length ? result[0] : null;
+	const query = `SELECT * from da.users WHERE sub = ?;`;
+	const params = [sub];
+	const result = await database.connectToRds(query, params);
+	return result?.length ? result[0] : null;
 };
 
 const createUser = async (sub, role) => {
-	const query = `INSERT INTO users (sub, role) VALUES ("${sub}", "${role}")`;
-	return await database.dbConn(query);
+	const query = `INSERT INTO users (sub, role) VALUES (?, ?)`;
+	const params = [sub, role];
+	return await database.connectToRds(query, params);
 };
 const users = { getUsers, getUserBySub, createUser };
 
